55eaf1eed27eb8f9907e5abb60ede60d484ae87f
Connor <cdy@email.sc.edu>
2018-07-24 03:56:13 -0400

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,70 @@
+// listen on port so now.sh likes it
+const https = require("https");
+const http = require("http");
+
+const Twit = require("twit");
+const config = require("./config");
+
+const bot = new Twit(config.twitterKeys);
+const googleImages = require("google-images");
+const images = new googleImages("010610163523097002920:bnksqwov1vq", "AIzaSyC48tH3-fCqgDrOOJ7Z4nqRXu8s6ZNqAFc");
+
+const tweet = require("./api/tweet");
+const path = require("path");
+const fs = require("fs");
+
+const msHour = 1000*60*60;
+
+function getRandomLine(filename)
+{
+  var data = fs.readFileSync(filename, "utf8");
+  var lines = data.toString().split("\n");
+  return lines[Math.floor(Math.random()*lines.length)];
+}
+
+function makeTweet()
+{
+  // Pick a random Christian descriptor
+  var desc = getRandomLine("./src/desc.txt");
+  desc = desc.replace(/^\w/, c => c.toUpperCase());
+  console.log("Random descriptor chosen: "+desc);
+
+  // Find an image for the descriptor
+  var res = images.search(desc, {size: "medium", safe: "high"});
+  res.then((results) => {
+  	var url = results[0]["url"];
+
+  	// Download the file
+  	console.log("Attempting to download file: "+url);
+  	var file = fs.createWriteStream("image.jpg");
+
+  	function handleGet(response)
+	{
+		// Pipe the response to the file
+		response.pipe(file);
+
+		// Wait for the file to finish downloading
+		file.on("finish", () => {
+			// Tweet it!
+			var filePath = path.join(__dirname, "../image.jpg");
+			tweet.tweetIMG(desc+" Christian", filePath);
+		});
+	}
+
+  	var request;
+  	if (url.substr(0,8).localeCompare("https://") == 0) request = https.get(url, handleGet);
+  	else request = http.get(url, handleGet);
+  }, (err) => {
+  	console.log(err);
+  });
+}
+
+/***
+* Code to run
+***/
+
+console.log("Bot starting...");
+
+// Run every 3 hours
+makeTweet();
+setInterval(makeTweet, msHour*3);
